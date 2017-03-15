24d1ce3130f4284c032c0e19519b21dd9c449edd
GaganTut <gstut@live.com>
2017-03-14 15:38:32 -1000

    created calculatorModule function

--- calculator.js
+++ calculator.js
@@ -1,65 +1,72 @@
 /**
- * Declare a function named `calculatorModule`
- * this function will have two private variables declared inside of it.
- * @variable PRIVATE { Number } `memory`
- * @variable PRIVATE { Number } `total`
- * @return {object} `calculator` object that can be used
+* Declare a function named `calculatorModule`
+* this function will have two private variables declared inside of it.
+* @variable PRIVATE { Number } `memory`
+* @variable PRIVATE { Number } `total`
+* @return {object} `calculator` object that can be used
+*/
+
+var calculatorModule = (function() {
+  var memory;
+  var total;
+  var calculator = {};
+
+  return calculator;
+})();
+
+/**
+ * sets the `total` to the number passed in
+ * @param  { Number } x
+ * @return { Number }    current total
  */
 
 
-  /**
-   * sets the `total` to the number passed in
-   * @param  { Number } x
-   * @return { Number }    current total
-   */
+/**
+ * Return the value of `total`
+ * @return { Number }
+ */
 
 
-  /**
-   * Return the value of `total`
-   * @return { Number }
-   */
+/**
+ * Sums the value passed in with `total`
+ * @param { Number } x
+ */
 
 
-  /**
-   * Sums the value passed in with `total`
-   * @param { Number } x
-   */
+/**
+ * Subtracts the value passed in from `total`
+ * @param  { Number } x
+ */
 
 
-  /**
-   * Subtracts the value passed in from `total`
-   * @param  { Number } x
-   */
+/**
+ * Multiplies the value by `total`
+ * @param  { Number } x
+ */
 
 
-  /**
-   * Multiplies the value by `total`
-   * @param  { Number } x
-   */
+/**
+ * Divides the value passing in by `total`
+ * @param  { Number } x
+ */
 
 
-  /**
-   * Divides the value passing in by `total`
-   * @param  { Number } x
-   */
+/**
+ * Return the value stored at `memory`
+ * @return { Number }
+ */
 
 
-  /**
-   * Return the value stored at `memory`
-   * @return { Number }
-   */
+/**
+ * Stores the value of `total` to `memory`
+ */
 
 
-  /**
-   * Stores the value of `total` to `memory`
-   */
+/**
+ * Clear the value stored at `memory`
+ */
 
-
-  /**
-   * Clear the value stored at `memory`
-   */
-
-  /**
-   * Validation
-   */
+/**
+ * Validation
+ */
 
